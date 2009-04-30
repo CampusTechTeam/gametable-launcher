ec0f010858f8b83651f11a5d57f194bb7f2ba12d
mh <mh@libavg.de>
2009-04-30 22:37:49 +0000

    appChooser/start.sh: set AVG_DEPLOY, start synthesizer
    svn path=/trunk/avg_media/mtc/appChooser/; revision=3795

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+export AVG_DEPLOY=1
 cd `dirname $0`
 export SC_JACK_DEFAULT_OUTPUTS="alsa_pcm:playback_1,alsa_pcm:playback_2"
 while [ true ]; do
-    #killall -9 scsynth
-    #scsynth -u 57110 -b 1026 2>/dev/null &
-    #sleep 6
+    killall -9 scsynth
+    scsynth -u 57110 -b 1026 2>/dev/null &
+    sleep 6
     ./appChooser.py >> appChooser.log 2>&1
-    #killall scsynth
+    killall scsynth
     sleep 1
 done
